3c8e6dbb0f53fdba530cb723818b659b1cc52d5f
arnursultan <badasspubgm@gmail.com>
2025-03-18 05:09:42 +0600

    Users, Products, need fix Stores and Orders

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, ProductImageViewSet
 
-# Используем Router для ViewSet'ов
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'product-images', ProductImageViewSet, basename='product-image')
