2d05e787f85e2006a5a2688bbf0c23764aba0ef5
Toktobaev Nooruzbek <nokatoktobaev@gmail.com>
2025-03-26 03:50:22 +0500

    Покрыл тестами и исправил баги которые вышли во время теста

--- apps/chats/serializers.py
+++ apps/chats/serializers.py
@@ -5,6 +5,8 @@ class MessageSerializer(serializers.ModelSerializer):
     sender_email = serializers.EmailField(source='sender.email', read_only=True)
     receiver_email = serializers.EmailField(source='receiver.email', read_only=True)
 
+    type = serializers.ChoiceField(choices=Message.TYPE_CHOICES)  # делаем поле обязательным
+
     class Meta:
         model = Message
         fields = [
@@ -12,6 +14,19 @@ class MessageSerializer(serializers.ModelSerializer):
             'type', 'text', 'file', 'is_read', 'timestamp'
         ]
         read_only_fields = ['sender', 'is_read', 'timestamp']
+
+    def validate(self, data):
+        msg_type = data.get('type')
+        text = data.get('text')
+        file = data.get('file')
+
+        if msg_type == Message.TEXT and not text:
+            raise serializers.ValidationError({"text": "Обязательное поле для текстового сообщения."})
+
+        if msg_type in [Message.IMAGE, Message.VIDEO, Message.FILE] and not file:
+            raise serializers.ValidationError({"file": "Файл обязателен для данного типа сообщения."})
+
+        return data
 
     def create(self, validated_data):
         validated_data['sender'] = self.context['request'].user
